e8189325253a5e999768d561303ecb47a4859615
Yaroslav <git@unimarijo.com>
2018-06-03 15:40:19 +0300

    Changed Bot Token, Channel IDs

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-botToken = "567318954:AAGW_6A__jFxEq2gBDpVY9C5jrlszuQd3EA"
+botToken = "601202629:AAGre0DlwhcM-NHqB1Aht6T6V6OJuuZR7kk"
 
-botChannelID = "-1001311491921"
-# Основной -- -1001201068931
-# Тестирование -- -1001311491921
+botChannelID = "-1001217496073"
+# Основной -- -1001338744519
+# Тестирование -- -1001217496073
 
-vkAccessToken = "f03e030243c9b420e1fc634bd6413ffd64bb52345e8af3db1a6083dc2d3c850d6ca0a455e6237fd9cfe03"
+vkAccessTokens = "f03e030243c9b420e1fc634bd6413ffd64bb52345e8af3db1a6083dc2d3c850d6ca0a455e6237fd9cfe03"
 
 vkGroupsIDs = ["73247559"]
