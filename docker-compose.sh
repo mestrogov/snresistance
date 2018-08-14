69f006312687a98a9a9163236181370e984a1610
Yaroslav <git@unimarijo.com>
2018-08-14 14:17:18 +0300

    For at least now, passwords won't be used for composed PSQL & Redis

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .configuration; rm -rf docker-compose.yml; envsubst < ".compose.yml" > "docker-compose.yml"
-sudo docker rm -f $(sudo docker ps -q -f name=SNResistance); docker-compose up -d
+docker rm -f $(docker ps -q -f name=SNResistance); docker-compose up -d
